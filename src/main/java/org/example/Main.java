3ed06f07c0761ce08d118520582a543e4ae0cb70
hwshin1 <hwshin5044@gmail.com>
2025-03-20 16:29:24 +0900

    2. 양의 정수 n을 요청받아 숫자 패턴의 피라미드를 출력하는 프로그램을 구현합니다. 피라미드는 n 레벨을 가져야 하며, 최상위 레벨에서 n부터 시작하여 맨 아래층에서 1까지 숫자가 감소해야 합니다

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
 
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                System.out.print('*');
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; n--) {
+            for (int j = n; j >= 1; j--) {
+                System.out.print(j);
             }
             System.out.println();
         }
